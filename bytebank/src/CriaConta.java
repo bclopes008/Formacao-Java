ec881d9da403f41797c15f79881a605d94afc8b3
Bruno Costa Lopes <bclopes008@gmail.com>
2021-04-03 21:09:29 -0300

    Java OO: Introdução à Orientação a Objetos - Aula 01 - Introdução à Orientação a Objetos

---
+++ bytebank/src/CriaConta.java
@@ -0,0 +1,21 @@
+public class CriaConta {
+	
+	public static void main(String[] args) {
+		
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println("O saldo da primeira conta é " + primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;
+		System.out.println("O saldo atualizado da primeira conta é " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta;
+		segundaConta.saldo = 50;
+		System.out.println("O saldo atualizado da primeira conta é " + primeiraConta.saldo);
+		System.out.println("O saldo atualizado da segunda conta é " + segundaConta.saldo);
+		
+		System.out.println(primeiraConta);
+		System.out.println(segundaConta);
+	}
+
+}
