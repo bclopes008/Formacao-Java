5c982776aba5b8afc91d5602f52c61910a2077f5
Bruno Costa Lopes <bclopes008@gmail.com>
2021-04-09 19:19:50 -0300

    Aula 04 - Composicação de objetos

---
+++ conta/src/TestaMetodos.java
@@ -0,0 +1,16 @@
+
+public class TestaMetodos {
+	
+	public static void main(String[] args) {
+		
+		Conta contaDaMarcela = new Conta();
+
+		contaDaMarcela.titular = new Cliente();
+		
+		contaDaMarcela.titular.nome = "Marcela";
+		
+		System.out.println(contaDaMarcela.titular.nome);
+		
+	}
+
+}
