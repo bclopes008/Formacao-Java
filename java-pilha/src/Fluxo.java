a33eb3e4deba065b252496951b9c4ac89e1324e2
Bruno Costa Lopes <bclopes008@gmail.com>
2021-05-20 20:51:25 -0300

    Aula 02 - Tratamento de exeções

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -3,7 +3,12 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
+        try {
         metodo1();
+        } catch(ArithmeticException | NullPointerException ex) {
+        	String msg = ex.getMessage();
+        	System.out.println("Exception " + msg);
+        }
         System.out.println("Fim do main");
     }
 
@@ -17,6 +22,8 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+        	Conta c = null;
+        	c.deposita();
         }
         System.out.println("Fim do metodo2");        
     }
